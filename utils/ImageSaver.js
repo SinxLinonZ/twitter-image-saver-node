53d72800aad0f38e968e77897baa037374842bf5
SinxLinonZ <urara.kbys@gmail.com>
2022-08-26 12:11:57 +0900

    Added: fetch multiply tweet function

--- utils/ImageSaver.js
+++ utils/ImageSaver.js
@@ -23,6 +23,11 @@ module.exports = {
 
                 const total = data.data.length;
                 let counter = 0;
+
+                // let tweetIdList = [];
+                // for (const tweet of data.data) {
+                // tweetIdList.push(tweet.id);
+                // }
                 for (const tweet of data.data) {
                     this.saveTwitterImage(tweet.id, recursive)
                         .then(() => {
@@ -48,6 +53,112 @@ module.exports = {
             });
         });
     },
+
+    // saveTwitterImages: function (tweetIdList, recursive = false) {
+    //     const filter = {
+    //         "tweet.fields": [
+    //             "created_at",
+    //             "entities",
+    //         ].join(','),
+    //         "expansions": [
+    //             "attachments.media_keys",
+    //             "author_id",
+    //             "referenced_tweets.id"
+    //         ].join(','),
+    //         "media.fields": [
+    //             "type",
+    //             "url"
+    //         ].join(','),
+    //         "user.fields": [
+    //             "name"
+    //         ].join(','),
+    //     };
+
+    //     return new Promise((resolve, reject) => {
+    //         TwitterParser.getTweets(tweetIdList, filter)
+    //             .then(_data => {
+
+    //                 for (const data of _data.data) {
+
+    //                     if (!data.includes) { resolve(); return; }
+    //                     if (!data.includes.media) {
+    //                         console.log("No media found");
+    //                         resolve();
+    //                         return;
+    //                     }
+    //                     if (data.errors || data.status == 429) {
+    //                         console.log("Rate limit exceeded");
+    //                         resolve();
+    //                         return;
+    //                     }
+
+    //                     if (data.data.referenced_tweets?.length > 0) {
+    //                         for (const tweet of data.data.referenced_tweets) {
+    //                             if (tweet.type == "retweeted") {
+    //                                 console.log("Retweet detected");
+    //                                 return this.saveTwitterImage(tweet.id, recursive);
+    //                             }
+    //                         }
+    //                     }
+
+    //                     for (const media of data.includes.media) {
+    //                         if (media.type == "photo") {
+    //                             // save photo
+    //                             console.log(media.url + "?name=orig");
+    //                             const fileName = media.url.split('/').pop();
+    //                             const file = fs.createWriteStream(process.env.root + '/saves/' + fileName);
+    //                             https.get(media.url + "?name=orig", (response) => {
+    //                                 response.pipe(file).on('close', () => {
+    //                                     console.log("Downloaded " + fileName);
+    //                                     file.close();
+
+    //                                     const ep = new exiftool.ExiftoolProcess(exiftoolBin);
+    //                                     ep
+    //                                         .open().then((pid) => console.log('Started exiftool process %s', pid))
+    //                                         .then(() => {
+    //                                             let StorylineIdentifier = [];
+    //                                             if (data.data.referenced_tweets?.length > 0) {
+    //                                                 for (const tweet of data.data.referenced_tweets) {
+    //                                                     if (tweet.type == "replied_to") {
+    //                                                         StorylineIdentifier.push(tweet.id);
+    //                                                         if (recursive) {
+    //                                                             console.log("Saving refrence: " + tweet.id);
+    //                                                             this.saveTwitterImage(tweet.id, true);
+    //                                                         }
+    //                                                     }
+    //                                                 }
+    //                                             }
+
+    //                                             ep.writeMetadata(process.env.root + '/saves/' + fileName, {
+    //                                                 all: '',
+
+    //                                                 "ArtworkDateCreated": data.data.created_at,
+    //                                                 "ArtworkCreator": data.includes.users[0].name,
+    //                                                 "ArtworkCreatorID": `@${data.includes.users[0].username}`,
+    //                                                 "ArtworkContentDescription": data.data.text.split('\n').join('<br>'),
+    //                                                 "ArtworkSource": data.data.id,
+    //                                                 "StorylineIdentifier": StorylineIdentifier.length > 0 ?
+    //                                                     StorylineIdentifier.join('<br>') : "none",
+    //                                                 "GenreCvId+": data.data.entities.hashtags ?
+    //                                                     data.data.entities.hashtags.map(hashtag => hashtag.tag).join("<br>") : "",
+
+    //                                             }, ['overwrite_original', 'codedcharacterset=utf8'])
+    //                                         })
+    //                                         .then(null, console.error)
+    //                                         // .then(() => ep.readMetadata(process.env.root + '/saves/' + fileName, ['-File:all']))
+    //                                         // .then(console.log, console.error)
+    //                                         .then(() => ep.close())
+    //                                         .then(() => { resolve() })
+    //                                         .catch(console.error)
+    //                                 });
+    //                             });
+    //                         }
+    //                     }
+    //                 }
+
+    //             });
+    //     });
+    // },
 
     saveTwitterImage: function (tweetId, recursive = false) {
         const filter = {
--- utils/TwitterParser.js
+++ utils/TwitterParser.js
@@ -1,6 +1,32 @@
 const https = require('https');
 
 module.exports = {
+    getTweets: function (tweetIdList, options) {
+        let url = "https://api.twitter.com/2/tweets";
+        let query = "?ids=" + tweetIdList.join(",");
+        if (options) {
+            query += "&" + Object.keys(options).map(function (key) {
+                return key + "=" + options[key];
+            }).join("&");
+        }
+        return new Promise((resolve, reject) => {
+            https.get(url + query, {
+                headers: {
+                    Authorization: "Bearer " + process.env.TWITTER_BEARER_TOKEN
+                }
+            }, (res) => {
+                let body = '';
+                res.on('data', (chunk) => {
+                    body += chunk;
+                }).on('end', () => {
+                    resolve(JSON.parse(body));
+                }).on('error', (err) => {
+                    reject(err);
+                });
+            });
+        });
+    },
+    
     getTweet: function (tweetId, options) {
         let url = "https://api.twitter.com/2/tweets/" + tweetId;
         let query = "";
